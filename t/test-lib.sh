7ed121f85252e1a146a66e07b58878e0fbc579ff
Felipe Contreras <felipe.contreras@gmail.com>
2023-04-01 05:07:10 -0600

    test: simplify author stuff
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- t/test-lib.sh
+++ t/test-lib.sh
@@ -2,12 +2,11 @@
 
 . "$(dirname "$0")"/sharness.sh
 
-GIT_AUTHOR_EMAIL=author@example.com
-GIT_AUTHOR_NAME='A U Thor'
-GIT_COMMITTER_EMAIL=committer@example.com
-GIT_COMMITTER_NAME='C O Mitter'
-export GIT_AUTHOR_EMAIL GIT_AUTHOR_NAME
-export GIT_COMMITTER_EMAIL GIT_COMMITTER_NAME
+cat > "$HOME/.gitconfig" <<-EOF
+[user]
+	name = Author
+	email = author@example.com
+EOF
 
 setup () {
 	git init -q &&
